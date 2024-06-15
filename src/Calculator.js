7f8589f3fb235ff69fdd5d80031ff1a265a5a726
Hossein <hossienbehzadi1381@gmail.com>
2024-06-15 17:24:44 +0330

    add functionality and bug handling of calculator

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,28 +1,72 @@
+import {useState} from 'react'
 import './Calculator.css'
 
 function Calculator(){
+
+    let [result, setResult] = useState('')
+    let [hasdot, setHasDot] = useState(false)
+
+    let operators = ['*', '+', '-', '/']
+
+    const changeInput = (text) => {
+        if (text==='÷') return '/'
+        if (text==='×') return '*'
+        return text
+    }
+
+    const clickHandler = (e) => {
+        let input = changeInput(e.target.innerText)
+        if (input === '.') {
+            if (hasdot) return
+            else{
+                setHasDot(true)
+            }
+        } else if (operators.includes(input)){
+            setHasDot(false)
+        }
+        setResult(result + input)
+    }
+
+
+    const clearBtn = () => {
+        setResult('')
+        setHasDot(false)
+    }
+
+    const deleteBtn = () => {
+        if(result.endsWith('.')){
+            setHasDot(false)
+        }
+        setResult(result.slice(0,-1))
+    }
+
+    const equalBtn = () => {
+        setResult(String(eval(result)))
+    }
+
+
     return(
         <div className="container">
-            <div className="screen"></div>
+            <div className="screen">{result}</div>
             <div className="buttons">
-                <button className='color two'>Clear</button>
-                <button className='color'>C</button>
-                <button className='color'>÷</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button className='color'>×</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button className='color'>-</button>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button className='color'>+</button>
-                <button>0</button>
-                <button>.</button>
-                <button className='color two'>=</button>
+                <button onClick={clearBtn} className='color two'>Clear</button>
+                <button onClick={deleteBtn} className='color'>D</button>
+                <button onClick={clickHandler} className='color'>÷</button>
+                <button onClick={clickHandler}>7</button>
+                <button onClick={clickHandler}>8</button>
+                <button onClick={clickHandler}>9</button>
+                <button onClick={clickHandler} className='color'>×</button>
+                <button onClick={clickHandler}>4</button>
+                <button onClick={clickHandler}>5</button>
+                <button onClick={clickHandler}>6</button>
+                <button onClick={clickHandler} className='color'>-</button>
+                <button onClick={clickHandler}>1</button>
+                <button onClick={clickHandler}>2</button>
+                <button onClick={clickHandler}>3</button>
+                <button onClick={clickHandler} className='color'>+</button>
+                <button onClick={clickHandler}>0</button>
+                <button onClick={clickHandler}>.</button>
+                <button onClick={equalBtn} className='color two'>=</button>
             </div>
         </div>
     )
